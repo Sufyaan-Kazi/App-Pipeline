b0eca1fd9728707f74d50442a08d944ba40e50b4
Sufyaan Kazi <skazi@pivotal.io>
2015-06-20 20:17:58 +0100

    Updated it to work in Jenkins jobs

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 set -e
 
 ## This script is used to test the BASH script commands
@@ -6,61 +6,7 @@ set -e
 ## This script is based on previous version written by Matt Stine and Jamie O'Meara
 ## Author - Sufyaan Kazi - Pivotal
 
-usage ()
-{
-  echo 'Usage : Script -u <user> -pw <password> -o <org> -s <space>'
-  echo '               -d <domain> -ap <app_prefix> -v <build_version>'
-  echo '               -sn <serviceName> -m <memory> -p <path_to_app> -i <instances>'
-  echo ' e.g. ./deployment.sh -u suf -pw ******** -o suf-org -s dev -d emea.fe.pivotal.io -ap cities-ui -sn citiesService -m 512m -p artifacts/cities-ui.jar i 1 -v 9'
-  exit
-}
-
-if [ "$#" -eq 0 ]
-then
-  usage
-fi
-
-## Parameters used to test the BASH script
-## They will be provided by Jenkins plugins,
-## Jenkins environment variables or job parameters
-while [ "$1" != "" ]; do
-case $1 in
-        -u )           shift
-                       CF_USER=$1
-                       ;;
-        -pw )          shift
-                       CF_PASSWORD=$1
-                       ;;
-        -o )           shift
-                       CF_ORG=$1
-                       ;;
-        -s )           shift
-                       CF_SPACE=$1
-                       ;;
-        -d )           shift
-                       CF_DOMAIN=$1
-                       ;;
-        -ap )          shift
-                       APP_PREFIX=$1
-                       ;;
-        -v )           shift
-                       BUILD_VERSION=$1
-                       ;;
-        -sn )          shift
-                       SERVICE_NAME=$1
-                       ;;
-        -m )           shift
-                       MEMORY=$1
-                       ;;
-        -p )           shift
-                       APP_PATH=$1
-                       ;;
-        -i )           shift
-                       INSTANCES=$1
-                       ;;
-    esac
-    shift
-done
+## The following parameters must be set CF_USER CF_PASSWORD CF_ORG CF_SPACE CF_DOMAIN APP_PREFIX BUILD_VERSION SERVICE_NAME MEMORY APP_PATH INSTANCES
 
 if [ -z $APP_PATH ]
 then
@@ -89,25 +35,6 @@ cf --version
 cf login -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE -a https://api.$CF_DOMAIN --skip-ssl-validation
 
 # ^^^^^^^^^^^^^^^^^^^^ Commands for Jenkins Script ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-## These steps complete the following, they are the only required steps for the Jenkins Job
-##
-##   1. Determine the name of the deployed app. The naming convention for this
-##      app is map-build-BUILD_NUMBER, ex map-build-45. We assume only 1 previous build
-##      exist but this script can be modified to support multiple previous builds.
-##
-##   2. If an app is found by querying the list of deployed apps then unmap a
-##      convenient url, ex map-dev.cfapps.io, and delete the existing app. In this 
-##      situation we are ok with downtime as we deploy the new app in development.
-##   
-##   Note: route name must be unique in a foundation. If you use Pivotal Web Services
-##         you need to make sure your convenient URL is unique.
-##   
-##   3. Push the next released version, bind an existing Rabbit service to the app, map
-##      the convenient URL to the new instance and start the app.
-##
-##   4. Log out
-##
 
 DEPLOYED_APP_NAME=$(cf apps | grep $APP_PREFIX | tail -n 1 | cut -d" " -f1)
 #DEPLOYED_APP_NAME=$(cf apps | grep $APP_PREFIX | sed '2!d' | cut -d" " -f1)
