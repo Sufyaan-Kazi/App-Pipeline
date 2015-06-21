b2bd2ba4810389ebd16cbd00a234c72a8da3a5ed
Sufyaan Kazi <skazi@pivotal.io>
2015-06-21 12:24:47 +0100

    Corrected domain, api and unique token stuff

--- scripts/promotion.sh
+++ scripts/promotion.sh
@@ -6,12 +6,12 @@ set -e
 ## This script is based on previous version written by Matt Stine and Jamie O'Meara
 ## Author - Sufyaan Kazi - Pivotal
 
-## The following parameters must be set CF_USER CF_PASSWORD CF_ORG CF_SPACE CF_DOMAIN APP_PREFIX SERVICE_NAME MEMORY INSTANCES
+## The following parameters must be set CF_USER CF_PASSWORD CF_ORG CF_SPACE CF_API CF_DOMAIN APP_PREFIX SERVICE_NAME MEMORY INSTANCES SSL
 ## Note - APP_PATH is 'hardcoded' in this script and BUILD_VERSION should come form you Jenkins Build if you set it up right!
 
 if [ -z $APP_PATH ]
-  then
-    APP_PATH=artifacts/$APP_PREFIX-${BUILD_VERSION}.jar
+then
+  APP_PATH=artifacts/$APP_PREFIX-${BUILD_VERSION}.jar
 fi
 
 echo_msg () {
@@ -25,14 +25,16 @@ echo_msg () {
 
 ## Variables used during Jenkins Build Process
 APP_NAME=$APP_PREFIX-$BUILD_VERSION
-HOST_NAME=$APP_PREFIX-$CF_USER-$BUILD_VERSION
+UNIQUE=$(echo $CF_USER | cut -d "@" -f1)
+UNIQUE="$APP_PREFIX-$UNIQUE"
+HOST_NAME=$UNIQUE-$BUILD_VERSION
 
 ## Log into PCF endpoint - Provided via Jenkins Plugin
 echo_msg "Logging into Cloud Foundry"
 ## wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/latest/cf-linux-amd64.tgz
 ## tar -zxvf cf-linux-amd64.tgz
 cf --version
-cf login -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE -a https://api.$CF_DOMAIN --skip-ssl-validation
+cf login -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE -a https://$CF_API $SSL
 
 # ^^^^^^^^^^^^^^^^^^^^ Commands for Jenkins Script ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
@@ -42,7 +44,7 @@ if [ ! -z "$SERVICE_NAME" ]
   then
     cf bind-service $APP_NAME $SERVICE_NAME 
 fi
-cf map-route $APP_NAME $CF_DOMAIN -n $APP_PREFIX-$CF_USER
+cf map-route $APP_NAME $CF_DOMAIN -n $UNIQUE
 
 echo_msg "Starting Container & Microservice"
 cf start $APP_NAME
@@ -54,7 +56,7 @@ if [ ! -z "$DEPLOYED_APP_NAME" -a "$DEPLOYED_APP_NAME" != " " -a "$DEPLOYED_APP_
   cf scale "$DEPLOYED_APP_NAME" -i 1
   echo "Temp sleep in script for demo purposes only!!"
   sleep 10
-  cf unmap-route "$DEPLOYED_APP_NAME" $CF_DOMAIN -n $APP_PREFIX-$CF_USER
+  cf unmap-route "$DEPLOYED_APP_NAME" $CF_DOMAIN -n $UNIQUE
   cf delete "$DEPLOYED_APP_NAME" -f -r
 fi
 
