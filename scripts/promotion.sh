3be36012991d2e0eaf9621c1958031558cf582e9
Sufyaan Kazi <skazi@pivotal.io>
2015-06-21 14:50:32 +0100

    Corrected order and naming of blue/green steps

--- scripts/promotion.sh
+++ scripts/promotion.sh
@@ -25,9 +25,9 @@ echo_msg () {
 
 ## Variables used during Jenkins Build Process
 APP_NAME=$APP_PREFIX-$BUILD_VERSION
-UNIQUE=$(echo $CF_USER | cut -d "@" -f1)
-UNIQUE="$APP_PREFIX-$UNIQUE"
-HOST_NAME=$UNIQUE-$BUILD_VERSION
+PUBLIC_ROUTE=$(echo $CF_USER | cut -d "@" -f1)
+PUBLIC_ROUTE="$APP_PREFIX-$HOST_NAME"
+UNIQUE=$PUBLIC_ROUTE-$BUILD_VERSION
 
 ## Log into PCF endpoint - Provided via Jenkins Plugin
 echo_msg "Logging into Cloud Foundry"
@@ -39,30 +39,31 @@ cf login -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE -a https://$CF_API 
 # ^^^^^^^^^^^^^^^^^^^^ Commands for Jenkins Script ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 echo_msg "Pushing new Microservice"
-cf push $APP_NAME -p $APP_PATH -m $MEMORY -n $HOST_NAME -i 1 -t 180 --no-start
+cf push $APP_NAME -p $APP_PATH -m $MEMORY -n $UNIQUE -i 1 -t 180 --no-start
 if [ ! -z "$SERVICE_NAME" ]
   then
     cf bind-service $APP_NAME $SERVICE_NAME 
 fi
-cf map-route $APP_NAME $CF_DOMAIN -n $UNIQUE
 
 echo_msg "Starting Container & Microservice"
 cf start $APP_NAME
+
+echo "Temp sleep in script for demo purposes only!!"
+sleep 10
 
 echo_msg "Performing Blue Green Deployment"
 DEPLOYED_APP_NAME=$(cf apps | grep $APP_PREFIX | head -n 1 | cut -d" " -f1)
 if [ ! -z "$DEPLOYED_APP_NAME" -a "$DEPLOYED_APP_NAME" != " " -a "$DEPLOYED_APP_NAME" != "$APP_NAME" ]; then
   echo "Performing zero-downtime cutover to $BUILD_VERSION"
+  cf map-route $APP_NAME $CF_DOMAIN -n $PUBLIC_ROUTE
   cf scale "$DEPLOYED_APP_NAME" -i 1
-  echo "Temp sleep in script for demo purposes only!!"
-  sleep 10
-  cf unmap-route "$DEPLOYED_APP_NAME" $CF_DOMAIN -n $UNIQUE
+  if [ ! -z "$INSTANCES" ]
+    then
+      echo_msg "Scaling new version to $INSTANCES instances"
+      cf scale $APP_NAME -i $INSTANCES
+  fi 
+  cf unmap-route "$DEPLOYED_APP_NAME" $CF_DOMAIN -n $PUBLIC_ROUTE
   cf delete "$DEPLOYED_APP_NAME" -f -r
 fi
 
-echo_msg "Scaling new Version to $INSTANCES instances"
-if [ ! -z "$INSTANCES" ]
-  then
-    cf scale $APP_NAME -i $INSTANCES
-fi
 cf logout
